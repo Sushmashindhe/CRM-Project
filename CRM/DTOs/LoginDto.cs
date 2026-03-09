61fbfdf6ac7026b529bf2e35256063613e82ac50
shreyas27-r <shreyas27r@gmail.com>
2026-03-09 16:54:08 +0530

    Added manager login

---
+++ CRM/DTOs/LoginDto.cs
@@ -0,0 +1,11 @@
+﻿namespace CRM.DTOs
+{
+    public class LoginDto
+    {
+        
+            public string Email { get; set; } = null!;
+
+            public string Password { get; set; } = null!;
+        
+    }
+}
--- CRM/Program.cs
+++ CRM/Program.cs
@@ -1,11 +1,16 @@
-using Microsoft.EntityFrameworkCore;
 using CRM.Data;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+.AddJwtBearer();
 
 builder.Services.AddControllers();
 builder.Services.AddOpenApi();
