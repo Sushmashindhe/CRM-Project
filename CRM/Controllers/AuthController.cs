02f5b0d71a6069b2b94549c1e71244a0821b3cd5
Sushmashindhe <iamsushma2004@gmail.com>
2026-03-09 09:33:59 +0530

    Added html pages

---
+++ CRM/Controllers/AuthController.cs
@@ -0,0 +1,18 @@
+﻿using CRM.Models;
+using Microsoft.AspNetCore.Mvc;
+
+[ApiController]
+[Route("api/[controller]")]
+public class AuthController : ControllerBase
+{
+    [HttpPost("login")]
+    public IActionResult Login([FromBody] LoginRequest request)
+    {
+        if (request.Email == "admin@test.com" && request.Password == "1234")
+        {
+            return Ok(new { message = "Login successful" });
+        }
+
+        return Unauthorized(new { message = "Invalid credentials" });
+    }
+}
