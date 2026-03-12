bf604ac7cc59ec027590917931e52dd01ece406c
Sushmashindhe <iamsushma2004@gmail.com>
2026-03-12 10:18:03 +0530

    Updated the dashboard

--- CRM/Models/Employees.cs
+++ CRM/Models/Employees.cs
@@ -12,7 +12,7 @@
 
         public string EmployeeType { get; set; } = "";
 
-        public string Password { get; set; } = null!;
+        public string Password { get; set; } = "";
 
         public string Role { get; set; } = "Employee";
     }
