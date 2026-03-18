b7421177996f45c54b789a074f72e166f7b6e013
Sushmashindhe <iamsushma2004@gmail.com>
2026-03-18 09:18:32 +0530

    Updated phone

---
+++ CRM/Migrations/20260318034644_Phonenumber.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CRM.Migrations
+{
+    /// <inheritdoc />
+    public partial class Phonenumber : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Employees_Managers_ManagerId",
+                table: "Employees");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Employees_Managers_ManagerId",
+                table: "Employees",
+                column: "ManagerId",
+                principalTable: "Managers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.SetNull);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Employees_Managers_ManagerId",
+                table: "Employees");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Employees_Managers_ManagerId",
+                table: "Employees",
+                column: "ManagerId",
+                principalTable: "Managers",
+                principalColumn: "Id");
+        }
+    }
+}
